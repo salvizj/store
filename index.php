ded15cb0bf809430feecb99671fac9ae290d1cbe
salvizj <sjansons2003@gmail.com>
2024-06-15 17:07:22 +0300

    commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+$request = $_SERVER['REQUEST_URI'];
+$viewDir = '/src/views/';
+
+switch ($request) {
+    case '':
+    case '/':
+        require __DIR__ . $viewDir . 'home.php';
+        break;
+
+    case '/register':
+        require __DIR__ . $viewDir . 'auth/register.php';
+        break;
+
+
+    default:
+        http_response_code(404);
+        require __DIR__ . $viewDir . '404.php';
+}
